fd9a44f20308f129ec1bc0f6b9aa3f81eaee8832
Rolandono <55685876+Rolandono@users.noreply.github.com>
2021-11-26 11:00:26 +0100

    feat: create bed endpoint

---
+++ restbe/Controllers/BedsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restapi.Models;
+
+namespace restapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BedsController : ControllerBase
+    {
+        private readonly MainDbContext _context;
+
+        public BedsController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Beds
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bed>>> GetBed()
+        {
+            return await _context.Bed.ToListAsync();
+        }
+
+        // GET: api/Beds/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bed>> GetBed(int id)
+        {
+            var bed = await _context.Bed.FindAsync(id);
+
+            if (bed == null)
+            {
+                return NotFound();
+            }
+
+            return bed;
+        }
+
+        // PUT: api/Beds/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBed(int id, Bed bed)
+        {
+            if (id != bed.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bed).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BedExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Beds
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Bed>> PostBed(Bed bed)
+        {
+            _context.Bed.Add(bed);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBed", new { id = bed.Id }, bed);
+        }
+
+        // DELETE: api/Beds/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Bed>> DeleteBed(int id)
+        {
+            var bed = await _context.Bed.FindAsync(id);
+            if (bed == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bed.Remove(bed);
+            await _context.SaveChangesAsync();
+
+            return bed;
+        }
+
+        private bool BedExists(int id)
+        {
+            return _context.Bed.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ restbe/Models/Bed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restapi.Models
+{
+    public class Bed
+    {
+        public int Id { get; set; }
+        public float Width { get; set; }
+        public float Length{ get; set; }
+        public float Height { get; set; }
+        public string BrandName { get; set; }
+
+    }
+}
