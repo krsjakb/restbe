a665c24a07bb2df85de874cb2bb966902a078ad7
SirFrigyes <c.frigyes1997@gmail.com>
2021-11-26 11:03:21 +0100

    ...

---
+++ restbe/Models/Laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Models
+{
+    public class Laptop
+    {
+        public int ID { get; set; }
+        public string SerialName { get; set; }
+        public string Brand { get; set; }
+    }
+}
