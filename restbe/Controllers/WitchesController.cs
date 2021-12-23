a9fb8e81cdc93ac2bc225d36f273fbb312451965
Benke07 <72821116+Benke07@users.noreply.github.com>
2021-12-23 19:44:06 +0100

    DB1U1V

---
+++ restbe/Controllers/WitchesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restbe.Models;
+
+namespace restbe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WitchesController : ControllerBase
+    {
+        private readonly MainDbContext _context;
+
+        public WitchesController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Witches>>> GetWitch()
+        {
+            return await _context.Witches.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Witches>> GetWitc(int id)
+        {
+            var witch = await _context.Witches.FindAsync(id);
+
+            if (witch == null)
+            {
+                return NotFound();
+            }
+
+            return witch;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWitch(int id, Witches witch)
+        {
+            if (id != witch.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(witch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WitchExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+       
+        [HttpPost]
+        public async Task<ActionResult<Witches>> PostWitch(Witches witch)
+        {
+            _context.Witches.Add(witch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWitch", new { id = witch.Id }, witch);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Witches>> DeleteWitch(int id)
+        {
+            var witch = await _context.Witches.FindAsync(id);
+            if (witch == null)
+            {
+                return NotFound();
+            }
+
+            _context.Witches.Remove(witch);
+            await _context.SaveChangesAsync();
+
+            return witch;
+        }
+
+        private bool WitchExist(int id)
+        {
+            return _context.Witches.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ restbe/Models/Witches.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Models
+{
+    public class Witches
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string House { get; set; }
+        public string Patronus { get; set; }
+
+    }
+}
