d325e0481585cca1314cc59bbd223632e2259615
Vitéz Viktória <YGPYHS@tr.pte.hu>
2021-12-29 12:20:23 +0100

    YGPYHS

--- restbe/Models/Books.cs
+++ restbe/Models/Books.cs
@@ -8,7 +8,7 @@ namespace restbe.Models
     public class Books
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
     }
 }
