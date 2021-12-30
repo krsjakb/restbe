9fdbea9683eb1a286f1f1b64e34ae3d328681283
rumplerbalint <rumplerbalint@gmail.com>
2021-12-30 16:00:01 +0100

    Rumpler Balint OZ3F4T

---
+++ restbe/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace restbe.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
