a3a14b7ee3a9c950870a0b0f693df834f7a190bf
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-12-31 12:50:52 +0100

    new model and controller

---
+++ restbe/Models/JobAdvertisement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Models
+{
+    public class JobAdvertisement
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public String text { get; set; }
+        public String location { get; set; }
+    }
+}
