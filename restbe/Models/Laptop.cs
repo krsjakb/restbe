1733857b30ebd1723fea4a1c7c3f110c36aade4d
Gitt <62553283+GLaci93@users.noreply.github.com>
2022-01-03 15:33:30 +0100

    done

---
+++ restbe/Models/Laptop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Models
+{
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public int Ar { get; set; }
+        public string Szin { get; set; }
+    }
+}
--- restbe/Models/MainDbContext.cs
+++ restbe/Models/MainDbContext.cs
@@ -17,5 +17,6 @@ namespace restbe.Models
         public DbSet<CarBrands> CarBrands { get; set; }
 
         public DbSet<CarModel> CarModel { get; set; }
+        public DbSet<Laptop> Laptop { get; set; }
     }
 }
