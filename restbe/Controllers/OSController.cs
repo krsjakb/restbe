5aab7fbf2abe6ca52ae421a7f7168c2b924ce2af
Parallel99 <46271427+parallel99@users.noreply.github.com>
2022-01-03 15:07:30 +0100

    add working project

---
+++ restbe/Controllers/OSController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restbe.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OSController : ControllerBase
+    {
+        private readonly MainDbContext _context;
+
+        public OSController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OperatingSystem>>> GetOS()
+        {
+            return await _context.Os.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OperatingSystem>> GetOs(int id)
+        {
+            var os = await _context.Os.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (os == null)
+            {
+                return NotFound();
+            }
+
+            return os;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOS(int id, OperatingSystem os)
+        {
+            if (id != os.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(os).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<OperatingSystem>> PostOs(OperatingSystem os)
+        {
+            _context.Os.Add(os);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOs", new { id = os.Id }, os);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OperatingSystem>> DeleteOs(int id)
+        {
+            var os = await _context.Os.FindAsync(id);
+            if (os == null)
+            {
+                return NotFound();
+            }
+
+            _context.Os.Remove(os);
+            await _context.SaveChangesAsync();
+
+            return os;
+        }
+
+        private bool OsExists(int id)
+        {
+            return _context.Os.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ restbe/Models/OperatingSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace restbe.Models
+{
+    public class OperatingSystem
+    {
+        private DateTime _releaseDate;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime releaseDate 
+        { 
+            get { return _releaseDate.Date; }
+            set { _releaseDate = value.Date; }
+        }
+    }
+}
