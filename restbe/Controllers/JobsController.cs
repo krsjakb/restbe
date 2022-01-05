2881f659bbb9f1d3f29c3936fb8d5bc6624b5489
Baranyai Máté <mateb06@gmail.com>
2022-01-05 18:36:47 +0100

    Add Job CRUD

---
+++ restbe/Controllers/JobsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restbe.Models;
+
+namespace restbe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly MainDbContext _context;
+
+        public JobsController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Job
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Job>>> GetJob()
+        {
+            return await _context.Job.ToListAsync();
+        }
+
+        // GET: api/Job/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            var job = await _context.Job.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+        // PUT: api/Job/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, Job job)
+        {
+            if (id != job.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JobExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Job
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Job>> PostJob(Job job)
+        {
+            _context.Job.Add(job);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJob", new { id = job.Id }, job);
+        }
+
+        // DELETE: api/Job/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Job>> DeleteJob(int id)
+        {
+            var job = await _context.Job.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Job.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return job;
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Job.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ restbe/Models/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace restbe.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string About { get; set; }
+        public int Salary { get; set; }
+    }
+}
