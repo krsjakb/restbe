16727a04039df43dd3301fb8d77e254a94c6d614
szarkavera <79326365+szarkavera@users.noreply.github.com>
2022-01-05 15:45:23 +0100

    pencil crud

---
+++ restbe/Models/PencilModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restbe.Models
+{
+    public class PencilModel
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Length { get; set; }
+    }
+}
