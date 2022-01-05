3eebfe01e40d6092e22a4c6e39171e2bccb5371b
SalaMark <94897694+SalaMark@users.noreply.github.com>
2022-01-05 19:12:15 +0100

    creat hairdryer endpoint

---
+++ restbe/Models/HairDryer.cs
@@ -0,0 +1,11 @@
+﻿namespace restbe.Models
+{
+    public class HairDryer
+    {
+
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Manufacturer { get; set; }
+
+    }
+}
